ff8486e3537eef224e5343527f09c579b96641f4
Edi <79288178+ediprekpalaj@users.noreply.github.com>
2021-07-08 23:15:17 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBZXB_X5qfFQsYlZDx384oRmotZsR6MI38",
-    authDomain: "match-tinder-clone.firebaseapp.com",
-    projectId: "match-tinder-clone",
-    storageBucket: "match-tinder-clone.appspot.com",
-    messagingSenderId: "251574016394",
-    appId: "1:251574016394:web:e9c41623923f2753195073",
-    measurementId: "G-F46ZJ6LSDB"
+   //your keys here
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
